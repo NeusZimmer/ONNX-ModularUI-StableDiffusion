4e8d0c45eb1a70f9f847b08f75be5e8019075edd
NeusZimmer <94193584+NeusZimmer@users.noreply.github.com>
2023-11-15 02:24:22 +0100

    Update pipelines_engines.py

--- Engine/pipelines_engines.py
+++ Engine/pipelines_engines.py
@@ -97,8 +97,8 @@ class SchedulersConfig(Borg):
         self._model_path = model_path
         self._scheduler_name = scheduler_name
         #provider = Engine_Configuration().Scheduler_provider
-        provider = Engine_Configuration().Scheduler_provider[provider]
-        provider_options=Engine_Configuration().Scheduler_provider[provider_options]
+        provider = Engine_Configuration().Scheduler_provider['provider']
+        provider_options=Engine_Configuration().Scheduler_provider['provider_options']
         """match scheduler_name:
             case "PNDM":
                 scheduler = PNDMScheduler.from_pretrained(model_path, subfolder="scheduler",provider=provider)
